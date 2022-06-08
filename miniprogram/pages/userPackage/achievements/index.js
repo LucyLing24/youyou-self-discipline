054db080309f938618f6bf2aae586e98a27197d4
lucyling <lucyling24@qq.com>
2022-06-08 11:06:36 +0800

    add my achievements page

---
+++ miniprogram/pages/userPackage/achievements/index.js
@@ -0,0 +1,130 @@
+// pages/userPackage/achievements/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    achievements:[
+      {
+        flag:1,
+        name:"学习新星",
+        details:"智力总值达到10",
+        pic_url:"/resource/images/user/achievements/study1.png",
+        pic_url_nag:"/resource/images/user/achievements/study1-nag.png"
+      },
+      {
+        flag:0,
+        name:"图书馆之王",
+        details:"智力总值达到100",
+        pic_url:"/resource/images/user/achievements/study2.png",
+        pic_url_nag:"/resource/images/user/achievements/study2-nag.png"
+      },
+      {
+        flag:0,
+        name:"学神0_o",
+        details:"智力总值达到200",
+        pic_url:"/resource/images/user/achievements/study3.png",
+        pic_url_nag:"/resource/images/user/achievements/study3-nag.png"
+      },
+      {
+        flag:1,
+        name:"运动爱好者",
+        details:"体力总值达到20",
+        pic_url:"/resource/images/user/achievements/exercise1.png",
+        pic_url_nag:"/resource/images/user/achievements/exercise1-nag.png"
+      },
+      {
+        flag:1,
+        name:"运动健将",
+        details:"体力总值达到50",
+        pic_url:"/resource/images/user/achievements/exercise2.png",
+        pic_url_nag:"/resource/images/user/achievements/exercise2-nag.png"
+      },
+      {
+        flag:0,
+        name:"流水不腐",
+        details:"体力总值达到100",
+        pic_url:"/resource/images/user/achievements/exercise3.png",
+        pic_url_nag:"/resource/images/user/achievements/exercise3-nag.png"
+      },
+      {
+        flag:1,
+        name:"社交新星",
+        details:"魅力值达到20",
+        pic_url:"/resource/images/user/achievements/charm1.png",
+        pic_url_nag:"/resource/images/user/achievements/charm1-nag.png"
+      },
+      {
+        flag:0,
+        name:"众人皆知",
+        details:"魅力值达到50",
+        pic_url:"/resource/images/user/achievements/charm2.png",
+        pic_url_nag:"/resource/images/user/achievements/charm2-nag.png"
+      },
+      {
+        flag:0,
+        name:"社交名流",
+        details:"魅力值达到100",
+        pic_url:"/resource/images/user/achievements/charm3.png",
+        pic_url_nag:"/resource/images/user/achievements/charm3-nag.png"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
